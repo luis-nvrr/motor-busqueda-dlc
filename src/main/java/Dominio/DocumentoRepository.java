27175a1a8863e2d16298b47ecfadfa7eb47b5fb7
luis navarro <luisfnvrr@gmail.com>
2021-03-27 15:23:02 -0300

    adds documentoRepository interface

---
+++ src/main/java/Dominio/DocumentoRepository.java
@@ -0,0 +1,11 @@
+package Dominio;
+
+import java.util.Map;
+
+public interface DocumentoRepository {
+
+    Documento getDocumento();
+    Map<String, Documento> getDocumentos();
+    void saveDocumento(Documento documento);
+    void saveDocumentos(Map<String, Documento> documentos);
+}
