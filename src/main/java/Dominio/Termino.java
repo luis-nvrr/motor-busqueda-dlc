5a75e87b967c3333e087a4b2e7f270529466123f
luis navarro <luisfnvrr@gmail.com>
2021-04-03 21:20:48 -0300

    refactor: removes posteo

--- src/main/java/Dominio/Termino.java
+++ src/main/java/Dominio/Termino.java
@@ -1,21 +1,68 @@
 package Dominio;
 
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Termino {
 
+    private Map<String, Documento> posteo;
     private String termino;
-    private Posteo posteo;
+    private int cantidadDocumentos;
+    private int maximaFrecuenciaTermino;
 
 
     public Termino(String termino){
-        this.posteo = new Posteo();
+        this.posteo = new Hashtable<>();
+        this.cantidadDocumentos = 1;
+        this.maximaFrecuenciaTermino = 1;
         this.termino = termino;
     }
 
     public void agregarDocumento(String path){
-        this.posteo.agregarDocumento(path);
+        cantidadDocumentos++;
+        Documento recuperado = posteo.get(path);
+
+        if(recuperado == null){ agregarInexistente(path);}
+        else{
+            actualizarDocumentoExistente(recuperado);
+        }
+    }
+
+    private void agregarInexistente(String path){
+        Documento documento = new Documento(path);
+        agregarAPosteo(path, documento);
+    }
+
+    private void actualizarDocumentoExistente(Documento documento){
+        documento.sumarFrecuencia();
+        actualizarFrecuenciaMaxima(documento);
+        agregarAPosteo(documento.getPath(), documento);
+    }
+
+    private void actualizarFrecuenciaMaxima(Documento documento){
+        if (documento.getFrecuenciaTermino() > maximaFrecuenciaTermino){
+            maximaFrecuenciaTermino = documento.getFrecuenciaTermino();
+        }
+    }
+
+    private void agregarAPosteo(String path, Documento documento){
+        this.posteo.put(path, documento);
     }
 
     public String mostrarOrdenPosteo(){
-        return posteo.mostrarOrden();
+        posteo = Ordenador.sortByValue(posteo);
+
+        Iterator<Map.Entry<String, Documento>> it = posteo.entrySet().iterator();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while(it.hasNext()) {
+            stringBuilder.append("documento: ");
+            stringBuilder.append(it.next().getValue().getFrecuenciaTermino());
+            stringBuilder.append(" ");
+        }
+        stringBuilder.append("\n");
+        return stringBuilder.toString();
     }
+
 }
