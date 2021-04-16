eca6c9a6d7e747dd5ff85b013bbd80a254ceb6e8
luis navarro <luisfnvrr@gmail.com>
2021-04-16 11:27:21 -0300

    refactor: mueve rutas a dotenv

--- README.md
+++ README.md
@@ -11,6 +11,9 @@ Crear en la raiz del directorio un archivo .env:
 USUARIO_DB=nombreUsuario
 PASSWORD_DB=password
 DB=database
+DOCUMENTOS=ubicacionDocumentos
+STOPWORDS=ubicacionDeLasStopWords
+ARCHIVO=ubicacionArchivo (solo si se esta cargando un solo archivo)
 ```
 
 donde:
--- src/main/java/MainCargar.java
+++ src/main/java/MainCargar.java
@@ -1,20 +1,22 @@
 import Aplicacion.GestorIndexacion;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class MainCargar {
 
     public static void main(String[] args) {
         GestorIndexacion gestorIndexacion = new GestorIndexacion();
 
-        //String pathArchivo = "C:\\Users\\luis\\Downloads\\prueba\\00ws110.txt";
-        //String stopWordsPath = "C:\\Users\\luis\\Downloads\\DLC\\stopWords.txt";
-        String directorioPath = "C:\\Users\\luis\\Downloads\\DLC\\prueba";
+        Dotenv dotenv = Dotenv.load();
+        String stopWordsPath = dotenv.get("STOPWORDS");
+        String directorioPath = dotenv.get("DOCUMENTOS");
+        //String archivoPath = dotenv.get("ARCHIVO");
 
-        //gestorIndexacion.cargarStopWords(stopWordsPath);
+        gestorIndexacion.cargarStopWords(stopWordsPath);
         gestorIndexacion.cargarVocabularioDirectorio(directorioPath);
         //gestorIndexacion.cargarVocabularioArchivo(pathArchivo);
 
         //System.out.println(gestorIndexacion.mostrarVocabulario());
-        //System.out.println(gestorIndexacion.mostrarCantidadTerminosVocabulario());
+        System.out.println(gestorIndexacion.mostrarCantidadTerminosVocabulario());
         //System.out.println(gestorVisualizacion.mostrarOrdenPosteo());
     }
 }
